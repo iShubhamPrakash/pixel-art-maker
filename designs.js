0954cccf462a4e8b27e57916b674e29b0f29e2b3
Shubham Prakash <shubham.prakash2308@gmail.com>
2018-04-26 17:18:00 +0530

    added makeGrid function

--- designs.js
+++ designs.js
@@ -1,10 +1,41 @@
-// Select color input
-// Select size input
+   $(document).ready(function() {
+        //Select color input
+        let colorChoice = $('#colorPicker');
+        
+        console.log("Working Shubham!!");
+        console.log(colorChoice);
 
-// When size is submitted by the user, call makeGrid()
+        // Select size inputs
+       
 
-function makeGrid() {
+        //when user click submit buttoon, width and height value is taken and makeGrid() is called
+        
+        function setSize() {
+            let heightOfCanvas = $('#inputHeight').attr('value');
+            let widthOfCanvas = $('#inputWidth').attr('value');
 
-// Your code goes here!
+            console.log(heightOfCanvas);
+            console.log(widthOfCanvas);
+            makeGrid(heightOfCanvas, widthOfCanvas);
+        }
+       
+        $('#submit-button').click(setSize());
+        
+ console.log(heightOfCanvas);
+ console.log(widthOfCanvas);
+        //function defination of makeGrid();
+        function makeGrid(heightOfCanvas, widthOfCanvas) {
+            let canvas = $('#pixelCanvas');
 
-}
+            for (let i = 1; i <= heightOfCanvas; i++) {
+                canvas.append('<tr class="tableRow"> </tr>');
+            }
+
+            for (let j = 1; j <= widthOfCanvas; j++) {
+                $('.tableRow').append('<td class="tableColumn"> </td>');
+            }
+        }
+        
+        // When size is submitted by the user, call makeGrid()
+        
+   });
