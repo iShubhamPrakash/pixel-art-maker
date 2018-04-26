1fd837cc67a0800fa7ebf350aff23f5223a26215
Shubham Prakash <shubham.prakash2308@gmail.com>
2018-04-26 18:05:30 +0530

    Fix canvas bug

--- designs.js
+++ designs.js
@@ -6,36 +6,27 @@
         console.log(colorChoice);
 
         // Select size inputs
-       
-
-        //when user click submit buttoon, width and height value is taken and makeGrid() is called
-        
-        function setSize() {
             let heightOfCanvas = $('#inputHeight').attr('value');
             let widthOfCanvas = $('#inputWidth').attr('value');
 
             console.log(heightOfCanvas);
             console.log(widthOfCanvas);
-            makeGrid(heightOfCanvas, widthOfCanvas);
-        }
-       
-        $('#submit-button').click(setSize());
+
+        // When size is submitted by the user, call makeGrid()
+         $('#submit-button').click(makeGrid(heightOfCanvas, widthOfCanvas));
         
- console.log(heightOfCanvas);
- console.log(widthOfCanvas);
-        //function defination of makeGrid();
-        function makeGrid(heightOfCanvas, widthOfCanvas) {
+         function makeGrid(heightOfCanvas, widthOfCanvas) {
+
             let canvas = $('#pixelCanvas');
 
-            for (let i = 1; i <= heightOfCanvas; i++) {
-                canvas.append('<tr class="tableRow"> </tr>');
-            }
+            for (let i = 0; i < heightOfCanvas; i++){
+                 canvas.append('<tr class="tableRow"> </tr>');
+                 for (let j = 0; j < widthOfCanvas; j++) {
+                     $('.tableRow').append('<td class="tableColumn"> </td>');
+                 }
+             }
 
-            for (let j = 1; j <= widthOfCanvas; j++) {
-                $('.tableRow').append('<td class="tableColumn"> </td>');
-            }
-        }
-        
-        // When size is submitted by the user, call makeGrid()
+             
+         }
         
    });
---
+++ test.js
@@ -0,0 +1,11 @@
+let row;
+let column;
+for (let i = 0; i < 10; i++) {
+    console.log('\n');
+     row+=' * ';
+
+    for (let j = 0; j < 10; j++) {
+     column+=' * ';
+
+    }
+}
