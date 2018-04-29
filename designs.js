adadb5a47a1225e639107fdc69d3ad5c15c6de51
i-shubhamprakash <shubham.prakash2308@gmail.com>
2018-04-30 00:38:24 +0530

    design complete

--- designs.js
+++ designs.js
@@ -1,56 +1,56 @@
 //jQuery selector for submit button-
-let onSubmit=$('#sizePicker');
+let onSubmit = $('#sizePicker');
 //pressing submit button calls makeGrid function-
-onSubmit.submit(function(event){
-//Prevent default action of submit button-
-event.preventDefault();
-//checks if submit button working-
-console.log("Submit button working bro");
-//Call makeGrid to creates a grid of cells-
-makeGrid();
+onSubmit.submit(function (event) {
+    //Prevent default action of submit button-
+    event.preventDefault();
+    //checks if submit button working-
+    console.log("Submit button working bro");
+    //Call makeGrid to creates a grid of cells-
+    makeGrid();
 });
 
 //To creae a grid of specified dimension-
-function makeGrid(){
+function makeGrid() {
     //store the value of height
-    let gridHeight=$('#inputHeight').val();
+    let gridHeight = $('#inputHeight').val();
 
     //store the value of width
-    let gridWidth=$('#inputWidth').val();
+    let gridWidth = $('#inputWidth').val();
 
     //checks if makeGrid is called and log the value of grid dimension in console
     console.log("makeGrid call successful bro-");
-    console.log("Height="+gridHeight+" Width="+gridWidth);
+    console.log("Height=" + gridHeight + " Width=" + gridWidth);
 
     //jQuery selector for table
-    let canvas=$('#pixelCanvas');
+    let canvas = $('#pixelCanvas');
 
     //removes previous table cells
     canvas.children().remove();
-        for(i=0;i<gridHeight;i++){
-            //creates table rows
-            canvas.append('<tr></tr>');
-        }
-        //jQuery selector for created table row
-        row=$('tr');
-        for(j=0;j<gridWidth;j++){
-             //created table data
-            row.append('<td></td>');
-        }
+    for (i = 0; i < gridHeight; i++) {
+        //creates table rows
+        canvas.append('<tr></tr>');
+    }
+    //jQuery selector for created table row
+    row = $('tr');
+    for (j = 0; j < gridWidth; j++) {
+        //created table data
+        row.append('<td></td>');
+    }
 
 
     //select clicked cell            
     let cell = canvas.find('td');
-	// When td is single clicked,change color of td to selected color
-	cell.click(function() {
+    // When td is single clicked,change color of td to selected color
+    cell.click(function () {
         var color;
-        color = $("#colorPicker").val();  // Select color input
-        console.log("changeColor is running, color="+ color); //log color value
-		$(this).attr('bgcolor', color);
+        color = $("#colorPicker").val(); // Select color input
+        console.log("changeColor is running, color=" + color); //log color value
+        $(this).attr('bgcolor', color);
     });
-    
+
     //on double click change color back to white
-    cell.dblclick(function(){
+    cell.dblclick(function () {
         $(this).attr('bgcolor', 'white');
     });
 
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 let num="hello";
 let msg;
 for(i=1;i<5;i++){
-msg=`So, ${num} ${i}`
+msg="So, ${num} ${i}";
 console.log(msg);
 }
